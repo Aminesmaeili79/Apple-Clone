8c13e39fe848335ecd8ffd4aae477950adc64c21
Amin Esmaeili <aminesmaeili79@yahoo.com>
2024-12-19 19:27:46 +0200

    init commit

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,10 @@
+import { highlightsSlides } from "../constants";
+const VideoCarousel = () => {
+  return (
+    <>
+      <div className="flex items-center">{highlightsSlides}</div>
+    </>
+  );
+};
+
+export default VideoCarousel;
